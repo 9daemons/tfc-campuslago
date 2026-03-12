cc6632af7bf2436703f66b09ab22562a86742491
9daemons <enzo.espcor@gmail.com>
2026-03-12 17:45:03 +0100

    Set production API URL to campus-lago.onrender.com

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 const API_BASE = isLocal
   ? 'http://localhost:3000/api'
-  : 'https://RENDER_URL_AQUI.onrender.com/api';
+  : 'https://campus-lago.onrender.com/api';
 
 const ALLOWED_DOMAIN = '@educa.madrid.org';
